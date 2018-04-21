da9494274a7d3415ade08ce3c89bb9904d754e4f
Adrien de Peretti <adrien.deperetti.freelance@gmail.com>
2018-04-21 12:17:53 +0200

    Remove useless import from UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
 import { userProvider } from './user.provider';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [],
     controllers: [UserController],
     components: [userProvider, UserService],
     exports: [UserService]
