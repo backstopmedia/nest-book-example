7c32c321929fe1b39961f9dd96a5a9e43a105b08
Adrien de Peretti <adrien.deperetti.freelance@gmail.com>
2018-04-21 11:52:59 +0200

    Remove useless import

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,8 @@
-import {Module, NestModule, RequestMethod} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-import { MiddlewaresConsumer } from '@nestjs/common/interfaces/middlewares';
 import { userProvider } from './user.provider';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import * as passport from 'passport';
 
 
 @Module({
