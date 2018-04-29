ac1ecf1ff36ddfef715672c70e1be5e8252ee254
Adrien de Peretti <adrien.deperetti.freelance@gmail.com>
2018-04-29 15:37:08 +0200

    Support WsAdapter with the verifyClient method using a custom middleware

---
+++ src/shared/adapters/ws.adapter.ts
@@ -0,0 +1,45 @@
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/switchMap';
+import * as WebSocket from 'ws';
+import { INestApplication, WebSocketAdapter } from '@nestjs/common';
+import { Observable } from 'rxjs/Observable';
+import { UserModule } from '../../modules/user/user.module';
+import { UserService } from '../../modules/user/user.service';
+import { WsAuthenticationGatewayMiddleware } from '../middlewares/ws.authentication.gateway.middleware';
+import { MessageMappingProperties } from '@nestjs/websockets';
+
+export class WsAdapter implements WebSocketAdapter {
+    constructor(private app: INestApplication) { }
+
+    create(port: number) {
+        return new WebSocket.Server({
+            server: this.app.getHttpServer(),
+            verifyClient: ({ origin, secure, req }, next) => {
+                return (new WsAuthenticationGatewayMiddleware(this.app.select(UserModule).get(UserService))).resolve()(req, next);
+            }
+        });
+    }
+
+    bindClientConnect(server, callback: (...args: any[]) => void) {
+        server.on('connection', callback);
+    }
+
+    bindMessageHandlers(client: WebSocket, handlers: MessageMappingProperties[], process: (data) => Observable<any>) {
+        Observable.fromEvent(client, 'message')
+            .switchMap((buffer) => this.bindMessageHandler(buffer, handlers, process))
+            .filter((result) => !!result)
+            .subscribe((response) => client.send(JSON.stringify(response)));
+    }
+
+    bindMessageHandler(buffer, handlers: MessageMappingProperties[], process: (data) => Observable<any>): Observable<any> {
+        const data = JSON.parse(buffer.data);
+        const messageHandler = handlers.find((handler) => handler.message === data.type);
+        if (!messageHandler) {
+            return Observable.empty();
+        }
+        const { callback } = messageHandler;
+        return process(callback(data));
+    }
+}
---
+++ src/shared/middlewares/ws.authentication.gateway.middleware.ts
@@ -0,0 +1,27 @@
+import * as jwt from 'jsonwebtoken';
+import { GatewayMiddleware, WsException } from '@nestjs/websockets';
+import { Middleware } from '@nestjs/common';
+import { UserService } from '../../modules/user/user.service';
+
+@Middleware()
+export class WsAuthenticationGatewayMiddleware implements GatewayMiddleware {
+    constructor(private userService: UserService) { }
+    resolve() {
+        return (req, next) => {
+            const matches = req.url.match(/token=([^&].*)/);
+            req['token'] = matches && matches[1];
+
+            if (!req.token) {
+                throw new WsException('Missing token.');
+            }
+
+            return jwt.verify(req.token, 'secret', async (err, payload) => {
+                if (err) throw new WsException(err);
+
+                const user = await this.userService.findOne({ where: { email: payload.email }});
+                req.user = user;
+                return next(true);
+            });
+        }
+    }
+}
